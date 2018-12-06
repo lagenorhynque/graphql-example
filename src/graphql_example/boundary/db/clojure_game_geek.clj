27910ef8aae4e3f6daf7758c6d2f3e469d67cc3d
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-12-07 00:31:38 +0900

    Refactor using a boundary protocol

--- src/graphql_example/boundary/db/clojure_game_geek.clj
+++ src/graphql_example/boundary/db/clojure_game_geek.clj
@@ -1,59 +1,22 @@
-(ns graphql-example.boundary.db.clojure-game-geek)
+(ns graphql-example.boundary.db.clojure-game-geek
+  (:require [duct.database.sql]))
 
-(defn find-game-by-id
-  [db game-id]
-  (->> db
-       deref
-       :games
-       (filter #(= game-id (:id %)))
-       first))
+(defprotocol ClojureGameGeek
+  (find-game-by-id [db game-id])
+  (find-member-by-id [db member-id])
+  (list-designers-for-game [db game-id])
+  (list-games-for-designer [db designer-id])
+  (list-ratings-for-game [db game-id])
+  (list-ratings-for-member [db member-id])
+  (upsert-game-rating [db game-id member-id rating]))
 
-(defn find-member-by-id
-  [db member-id]
-  (->> db
-       deref
-       :members
-       (filter #(= member-id (:id %)))
-       first))
-
-(defn list-designers-for-game
-  [db game-id]
-  (let [designers (:designers (find-game-by-id db game-id))]
-    (->> db
-         deref
-         :designers
-         (filter #(contains? designers (:id %))))))
-
-(defn list-games-for-designer
-  [db designer-id]
-  (->> db
-       deref
-       :games
-       (filter #(-> % :designers (contains? designer-id)))))
-
-(defn list-ratings-for-game
-  [db game-id]
-  (->> db
-       deref
-       :ratings
-       (filter #(= game-id (:game_id %)))))
-
-(defn list-ratings-for-member
-  [db member-id]
-  (->> db
-       deref
-       :ratings
-       (filter #(= member-id (:member_id %)))))
-
-(defn ^:private apply-game-rating
-  [game-ratings game-id member-id rating]
-  (->> game-ratings
-       (remove #(and (= game-id (:game_id %))
-                     (= member-id (:member_id %))))
-       (cons {:game_id game-id
-              :member_id member-id
-              :rating rating})))
-
-(defn upsert-game-rating
-  [db game-id member-id rating]
-  (swap! db update :ratings apply-game-rating game-id member-id rating))
+(extend-protocol ClojureGameGeek
+  duct.database.sql.Boundary
+  ;; TODO: implement for SQL DB
+  (find-game-by-id [db game-id])
+  (find-member-by-id [db member-id])
+  (list-designers-for-game [db game-id])
+  (list-games-for-designer [db designer-id])
+  (list-ratings-for-game [db game-id])
+  (list-ratings-for-member [db member-id])
+  (upsert-game-rating [db game-id member-id rating]))
--- src/graphql_example/boundary/db/example_db.clj
+++ src/graphql_example/boundary/db/example_db.clj
@@ -1,14 +1,62 @@
 (ns graphql-example.boundary.db.example-db
   (:require [clojure.edn :as edn]
             [clojure.java.io :as io]
+            [graphql-example.boundary.db.clojure-game-geek :as db.clojure-game-geek]
             [integrant.core :as ig]))
+
+(defn- apply-game-rating [game-ratings game-id member-id rating]
+  (->> game-ratings
+       (remove #(and (= game-id (:game_id %))
+                     (= member-id (:member_id %))))
+       (cons {:game_id game-id
+              :member_id member-id
+              :rating rating})))
+
+(defrecord Boundary [spec]
+  db.clojure-game-geek/ClojureGameGeek
+  (find-game-by-id [{:keys [spec]} game-id]
+    (->> spec
+         deref
+         :games
+         (filter #(= game-id (:id %)))
+         first))
+  (find-member-by-id [{:keys [spec]} member-id]
+    (->> spec
+         deref
+         :members
+         (filter #(= member-id (:id %)))
+         first))
+  (list-designers-for-game [{:keys [spec] :as db} game-id]
+    (let [designers (:designers (db.clojure-game-geek/find-game-by-id db game-id))]
+      (->> spec
+           deref
+           :designers
+           (filter #(contains? designers (:id %))))))
+  (list-games-for-designer [{:keys [spec]} designer-id]
+    (->> spec
+         deref
+         :games
+         (filter #(-> % :designers (contains? designer-id)))))
+  (list-ratings-for-game [{:keys [spec]} game-id]
+    (->> spec
+         deref
+         :ratings
+         (filter #(= game-id (:game_id %)))))
+  (list-ratings-for-member [{:keys [spec]} member-id]
+    (->> spec
+         deref
+         :ratings
+         (filter #(= member-id (:member_id %)))))
+  (upsert-game-rating [{:keys [spec]} game-id member-id rating]
+    (swap! spec update :ratings apply-game-rating game-id member-id rating)))
 
 (defmethod ig/init-key ::db
   [_ _]
   (-> (io/resource "cgg-data.edn")
       slurp
       edn/read-string
-      atom))
+      atom
+      ->Boundary))
 
 (defmethod ig/halt-key! ::db
   [_ _]
